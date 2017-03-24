d0fcbba64c7428ebfbe3d2ea89f58be43ee90e25
Srivathsa Harish Venkataramanan <srivathsaharish@gmail.com>
2017-03-24 18:16:13 +0000

    Add project files.

---
+++ AcuCafe/AcuCafe.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace AcuCafe
+{
+    public class AcuCafe
+    {
+        public static Drink OrderDrink(string type, bool hasMilk, bool hasSugar)
+        {
+            Drink drink = new Drink();
+            if (type == "Expresso")
+            {
+                drink = new Expresso();
+            }
+            else if (type == "HotTea")
+                drink = new Tea();
+            else if (type == "IceTea")
+                drink = new IceTea();
+
+            try
+            {
+                drink.HasMilk = hasMilk;
+                drink.HasSugar = hasSugar;
+                drink.Prepare(type);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("We are unable to prepare your drink.");
+                System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());
+            }
+
+            return drink;
+        }
+    }
+
+    public class Drink
+    {
+        public const double MilkCost = 0.5;
+        public const double SugarCost = 0.5;
+
+        public bool HasMilk { get; set; }
+
+        public bool HasSugar { get; set; }
+        public string Description { get; }
+
+        public double Cost()
+        {
+            return 0;
+        }
+
+        public void Prepare(string drink)
+        {
+            string message = "We are preparing the following drink for you: " + Description;
+            if (HasMilk)
+                message += "with milk";
+            else
+                message += "without milk";
+
+            if (HasSugar)
+                message += "with sugar";
+            else
+                message += "without sugar";
+
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Expresso : Drink
+    {
+        public new string Description
+        {
+            get { return "Expresso"; }
+        }
+
+        public new double Cost()
+        {
+            double cost = 1.8;
+
+            if (HasMilk)
+                cost += MilkCost;
+
+            if (HasSugar)
+                cost += SugarCost;
+
+            return cost;
+        }
+    }
+
+    public class Tea : Drink
+    {
+        public new string Description
+        {
+            get { return "Hot tea"; }
+        }
+
+        public new double Cost()
+        {
+            double cost = 1;
+
+            if (HasMilk)
+                cost += MilkCost;
+
+            if (HasSugar)
+                cost += SugarCost;
+
+            return cost;
+        }
+    }
+
+    public class IceTea : Drink
+    {
+        public new string Description
+        {
+            get { return "Ice tea"; }
+        }
+
+        public new double Cost()
+        {
+            double cost = 1.5;
+
+            if (HasMilk)
+                cost += MilkCost;
+
+            if (HasSugar)
+                cost += SugarCost;
+
+            return cost;
+        }
+    }
+}
---
+++ AcuCafe/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿AcuCafe has an ordering system which is poorly designed and sometime fails to deliver the right orders. For example, some of the clients complaint that their ice tea had milk in it. Also, the system turned out to be a maintenance nightmare as it is difficult to add new drinks.
+
+AcuCafe has selected you to update their ordering system to be able to:
+-	Stop the preparation of an ice tea with milk and inform the barista
+-	Add a new chocolate topping to the expresso
+Can you think of any new requirements that can impact your design?
+
+Ensure you commit the completed code into GitHub and send us a link to a public repository. Please register a GitHub account if you don't already have one.
+Hints:
+We are looking for a solution that shows a good understanding of the SOLID principles, object oriented programming and that displays a working knowledge of TDD.
+Please add some commentary to justify the decisions you've taken, if possible by quoting books, blog posts or talks that have influenced you.

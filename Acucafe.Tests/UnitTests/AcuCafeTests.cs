e335d78eb3bb6f112c6e522462beff3ef55f27ad
Srivathsa Harish Venkataramanan <srivathsaharish@gmail.com>
2017-03-27 13:52:06 +0100

    Added Unit tests

---
+++ Acucafe.Tests/UnitTests/AcuCafeTests.cs
@@ -0,0 +1,53 @@
+﻿using Acucafe.Tests.UnitTests.Fixtures;
+using AcuCafe;
+using Moq;
+using Ploeh.AutoFixture.Xunit2;
+using Shouldly;
+using Xunit;
+
+namespace Acucafe.Tests.UnitTests
+{
+    public class AcuCafeTests
+    {
+        [Theory]
+        [InlineAutoMoqData(false, true, true, "We are unable to prepare your drink.")]
+        [InlineAutoMoqData(true, false, true, "We are unable to prepare your drink.")]
+        [InlineAutoMoqData(true, false, false, "Preparation Message.")]
+        public void OrderDrink_Should_Validate_And_Prepare_Order(bool validation, bool returnNullValidator, bool returnNullDrink,
+            string expectedMessage, Mock<Drink> expectedDrink,
+            [Frozen]Mock<IDrinkFactory> drinkFactory, [Frozen]Mock<IDrinkValidatorFactory> drinkValidatorFactory, [Frozen]Mock<IDrinkValidator> drinkValidator,
+            AcuCafe.AcuCafe sut)
+        {
+            if (!returnNullValidator)
+            {
+                drinkValidatorFactory.Setup(x => x.Get(It.IsAny<string>())).Returns(drinkValidator.Object);
+                drinkValidator.Setup(x => x.Validate(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(validation);
+            }
+            else
+            {
+                drinkValidatorFactory.Setup(x => x.Get(It.IsAny<string>())).Returns(default(IDrinkValidator));
+            }
+
+            if (!returnNullDrink)
+            {
+                drinkFactory.Setup(x => x.Get(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(expectedDrink.Object);
+                expectedDrink.Setup(x => x.Prepare()).Returns(expectedMessage);
+            }
+            else
+            {
+                drinkFactory.Setup(x => x.Get(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(default(Drink));
+            }
+
+            var result = sut.OrderDrink(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>());
+            result.Item1.ShouldBe(expectedMessage);
+            if (!returnNullDrink && !returnNullValidator)
+            {
+                result.Item2.ShouldNotBeNull();
+            }
+            else
+            {
+                result.Item2.ShouldBeNull();
+            }
+        }
+    }
+}
---
+++ Acucafe.Tests/UnitTests/InlineAutoMoqData.cs
@@ -0,0 +1,14 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Acucafe.Tests.UnitTests.Fixtures
+{
+    public class InlineAutoMoqDataAttribute : CompositeDataAttribute
+    {
+        public InlineAutoMoqDataAttribute(params object[] values)
+            : base(new InlineDataAttribute(values), new AutoMoqDataAttribute())
+        {
+        }
+
+    }
+}
